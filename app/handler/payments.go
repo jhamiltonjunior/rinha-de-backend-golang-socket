3b9470f7e6a830ba7b1ac84fbf9001fed9f65ef8
josehamiltonsantosjunior <josehamiltonsantosjunior@gmail.com>
2025-08-15 23:12:34 -0300

    retest

--- app/handler/payments.go
+++ app/handler/payments.go
@@ -79,7 +79,6 @@ func PaymentsSummary(path string) []byte {
 	path = strings.Split(path, " ")[0]
 	fmt.Println("PaymentsSummary called with path:", path)
 
-	time.Sleep(950 * time.Millisecond)
 	from := "1970-01-01T00:00:00.000Z"
 	to := "9999-12-31T23:59:00.000Z"
 
